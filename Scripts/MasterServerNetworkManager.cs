6d01a84fc0e0d911b36d48ca7b949f485e0129f5
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-20 14:57:42 +0700

    Add some empty skeleton classes

---
+++ Scripts/MasterServerNetworkManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MasterServerNetworkManager : NetworkManagerSimple
+{
+    protected override void RegisterServerMessages()
+    {
+        base.RegisterServerMessages();
+    }
+
+    protected override void RegisterClientMessages(NetworkClient client)
+    {
+        base.RegisterClientMessages(client);
+    }
+}
